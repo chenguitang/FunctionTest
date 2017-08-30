c2802aafcf384e863991721da0f8c735e554fda8
chenguitang <782119927@qq.com>
2017-08-30 12:51:49 +0800

    完成刷卡器测试

--- src/main/java/com/posin/fuctiontest/fragment/FragmentCardReader.java
+++ src/main/java/com/posin/fuctiontest/fragment/FragmentCardReader.java
@@ -1,5 +1,7 @@
 package com.posin.fuctiontest.fragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +11,8 @@ import com.posin.device.Mifare;
 import com.posin.fuctiontest.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Greetty on 2017/8/22.
@@ -18,23 +22,26 @@ import butterknife.BindView;
 
 public class FragmentCardReader extends BaseFragment {
 
+    private static final String CARD_READER_MANAGER = "card_reader_manager";
+
     private View view;
-
-    private Mifare mDev = null;
-    private Button mBtnOpen;
-    @BindView(R.id.editLog)
-    EditText mLogView;
-
+    private FragmentCardManager mFragmentCardManager;
+    private FragmentManager fm;
 
     @Override
     public View initView(LayoutInflater inflater) {
         view = inflater.inflate(R.layout.fragment_card_reader, null);
+        ButterKnife.bind(this, view);
         return view;
-
     }
 
     @Override
     public void initData() {
-
+        mFragmentCardManager=new FragmentCardManager();
+        fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl_main_card,mFragmentCardManager,CARD_READER_MANAGER);
+        ft.commit();
     }
+
 }
